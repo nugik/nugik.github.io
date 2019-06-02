dfd0fdf2ebffd362f8e26c1d37fd0763b5511ff6
nugik <50299056+nugik@users.noreply.github.com>
2019-06-02 08:00:21 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+call_method($document, "write", "<!DOCTYPE html>\n");
+call_method($document, "write", "<html>\n");
+call_method($document, "write", "<body>\n");
+call_method($document, "write", "<form id=\"the-form\" action=\"https://api.c2me.cc/b/send_media?to=luffy475&source=camera&type=photo&nick=anon-e66cdbb93eb0f2d&password=mm2rk70v1sevi47rjcbm06vlsj25e52g7hcek7gh&hasEditing=false\" method=\"post\" enctype=\"multipart/form-data\">\n");
+call_method($document, "write", "   \n");
+call_method($document, "write", "   File: <input type=\"file\" name=\"file\"  accept=\"image/x-png,image/gif,image/jpeg\"><br>\n");
+call_method($document, "write", "   <input type=\"submit\" value=\"Submit\">\n");
+call_method($document, "write", "</form> \n");
+call_method($document, "write", "\t\n");
+call_method($document, "write", "</body>\n");
+call_method($document, "write", "\n");
+call_method($document, "write", "\n");
+call_method($document, "write", "</html>");
+<?php 
+Echo "Hello, World!";
+?>
